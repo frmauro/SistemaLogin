4171574d36b72ffa09e14cde84a62a2da2131010
francisco <frmauro8@hotmail.com>
2020-08-27 18:27:39 -0300

    novas melhorias

--- src/br/com/SistemaLogin/login/CadastroLoginServlet.java
+++ src/br/com/SistemaLogin/login/CadastroLoginServlet.java
@@ -23,8 +23,7 @@ public class CadastroLoginServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		request.getRequestDispatcher("cadastrarLogin.jsp").forward(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
